458d7dbc2ec6e1ff3b3e73bd78afaa81aedc0ca7
LiamTarasa <liamtarasa@gmail.com>
2025-02-19 01:54:27 +0900

    modify to birdeye

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,21 @@ function App() {
 
   return (
     <div className='flex flex-col w-screen h-[600px] rounded-xl'>
-      {/* <iframe
+      <iframe
         id='tv-iframe'
         width='100%'
         height='100%'
         className='size-full object-cover'
         src={`https://birdeye.so/tv-widget/${tokenInfo?.addr}?chain=${tokenInfo?.chain}&chartType=CANDLE&chartInterval=1&chartLeftToolbar=show`}
         allowfullscreen={true}
-      ></iframe> */}
+      ></iframe>
 
-      <iframe
+      {/* <iframe
         className="w-full h-full"
         src={`https://www.geckoterminal.com/eth/pools/0xa3c2076eb97d573cc8842f1db1ecdf7b6f77ba27?embed=1&info=0&swaps=0&grayscale=0&light_chart=0`}
         allow="clipboard-write"
         allowfullscreen>
-      </iframe>
+      </iframe> */}
     </div >
   )
 }
